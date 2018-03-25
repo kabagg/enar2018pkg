5ced3d636b2496ad397348a88d1eae0cbb13f5cc
Keith Baggerly <kabagg@gmail.com>
2018-03-25 11:24:28 -0500

    initial posting of all package contents

---
+++ R/enar2018pkg.R
@@ -0,0 +1,8 @@
+#' Infrastructure Tools Package for ENAR 2018
+#'
+#' This package encapsulates various scripts and templates
+#' we've found to be useful in organizing projects.
+#'
+#' @docType package
+#' @name enar2018pkg
+NULL
